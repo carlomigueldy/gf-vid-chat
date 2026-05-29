d387c150195203401b430c0c4343239b7d94ca9b
Carlo Miguel Dy <carlomigueldy@gmail.com>
2026-05-30 06:18:46 +0800

    feat: self-host Fredoka + Plus Jakarta fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource-variable/fredoka'
+import '@fontsource-variable/plus-jakarta-sans'
 import './globals.css'
 import App from './App.tsx'
 
---
+++ src/types/fontsource.d.ts
@@ -0,0 +1,2 @@
+declare module '@fontsource-variable/fredoka'
+declare module '@fontsource-variable/plus-jakarta-sans'
